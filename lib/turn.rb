c5e05d12151f3e2a02cbc572ae8783c9b88ee0cb
Ignacio R Espino <irespino@capozios.com>
2018-07-17 22:56:53 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,8 @@
-display_board(board)
-  puts " "
+def display_board(aBoard)
+  puts " #{aBoard[0]} | #{aBoard[1]} | #{aBoard[2]} "
+  puts '-----------'
+  puts " #{aBoard[3]} | #{aBoard[4]} | #{aBoard[5]} "
+  puts '-----------'
+  puts " #{aBoard[6]} | #{aBoard[7]} | #{aBoard[8]} "
 end
+
